8f02e22db9dbc54182ff5f01f3dbe1fefd194a5c
kpetersn <kmpeters@anl.gov>
2019-04-15 14:19:28 -0500

    Improved the example IOC

--- iocs/mxmotorIOC/iocBoot/iocMXmotor/st.cmd
+++ iocs/mxmotorIOC/iocBoot/iocMXmotor/st.cmd
@@ -1,8 +1,5 @@
 #!../../bin/linux-x86_64/mxmotor
 
-## You may have to change mxmotor to something else
-## everywhere it appears in this file
-
 < envPaths
 
 cd "${TOP}"
@@ -11,11 +8,17 @@ cd "${TOP}"
 dbLoadDatabase "dbd/mxmotor.dbd"
 mxmotor_registerRecordDeviceDriver pdbbase
 
-## Load record instances
-#dbLoadRecords("db/xxx.db","user=kpetersn")
-
 cd "${TOP}/iocBoot/${IOC}"
+
+## motorUtil (allstop & alldone)
+dbLoadRecords("$(MOTOR)/db/motorUtil.db", "P=mxmotor:")
+
+##
+
+
 iocInit
 
-## Start any sequence programs
-#seq sncxxx,"user=kpetersn"
+## motorUtil (allstop & alldone)
+motorUtilInit("mxmotor:")
+
+# Boot complete
